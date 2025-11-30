34cb114b6c9e90b378c95fd324a9c2fa261ac903
greatit-lab <gil2zum@gmail.com>
2025-12-01 00:31:55 +0900

    Implement WaferController with multiple endpoints

---
+++ backend/src/wafer/wafer.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { WaferService, WaferQueryParams } from './wafer.service';
+
+@Controller('api/WaferData')
+export class WaferController {
+  constructor(private readonly waferService: WaferService) {}
+
+  @Get('flatdata')
+  getFlatData(@Query() query: WaferQueryParams) {
+    return this.waferService.getFlatData(query);
+  }
+
+  @Get('pointdata')
+  getPointData(@Query() query: WaferQueryParams) {
+    return this.waferService.getPointData(query);
+  }
+
+  @Get('statistics')
+  getStatistics(@Query() query: WaferQueryParams) {
+    return this.waferService.getStatistics(query);
+  }
+
+  @Get('checkpdf')
+  checkPdf(@Query() query: WaferQueryParams) {
+    return this.waferService.checkPdf(query);
+  }
+}
