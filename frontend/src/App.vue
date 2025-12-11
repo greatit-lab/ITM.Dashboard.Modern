7fe4dc61843b0edd3f832354834fa246a3afedfc
greatit-lab <gil2zum@gmail.com>
2025-12-11 13:43:52 +0900

    Update App.vue -- 1211_13

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,7 +11,7 @@
     >
       <Header />
 
-      <div class="flex-1 relative px-5 pb-0 pt-2">
+      <div class="relative flex-1 px-5 pt-2 pb-0">
         <router-view v-slot="{ Component }">
           <transition name="fade" mode="out-in">
             <component :is="Component" />
